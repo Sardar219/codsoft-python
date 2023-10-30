9fd8c2b1657ec3ca83f216a9b067d8512e99fbd1
Sardar <129367930+Sardar219@users.noreply.github.com>
2023-10-30 11:45:38 +0430

    Add files via upload

---
+++ CALCULATOR.py
@@ -0,0 +1,15 @@
+Number1=int(input("Enter The Number One : "))
+Number2=int(input("Enter The Number Two : "))
+oprator=input("Enter one oprator [+,-,x,/] : ")
+if oprator=="+":
+    print(Number1+Number2)
+elif oprator=="-":
+    print(Number1-Number2)
+elif oprator=="x":
+    print(Number1*Number2)
+elif oprator=="/":
+    print(Number1/Number2)
+else:
+    print("You enter a wrong oprator Try Agine")
+    
+    
